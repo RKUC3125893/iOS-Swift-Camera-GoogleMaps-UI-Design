a8391e27b34e88d16f2fd744291fd91ed2ef7039
iOS Development <iosdevelopment@rahuls-mbp.lan>
2017-01-02 11:20:27 +1100

    Initial Commit

---
+++ Map My Crash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map My Crash
+//
+//  Created by iOS Development on 2/1/17.
+//  Copyright © 2017 ios development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
